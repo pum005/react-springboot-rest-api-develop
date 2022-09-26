036e58d638a180a24baa3a05fd8269f47fda989a
si hwan <pum005@naver.com>
2022-09-26 20:28:21 +0900

    create model

---
+++ src/test/java/com/example/gccoffee/GccoffeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gccoffee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GccoffeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
