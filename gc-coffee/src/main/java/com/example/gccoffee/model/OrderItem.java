2333ac86e6827f7d0527754d170c41e95a571691
si hwan <pum005@naver.com>
2022-09-30 19:34:31 +0900

    split gc-coffee and kdt-react-order-ui

---
+++ gc-coffee/src/main/java/com/example/gccoffee/model/OrderItem.java
@@ -0,0 +1,5 @@
+package com.example.gccoffee.model;
+
+import java.util.UUID;
+public record OrderItem(UUID productId, Category category, long price, int quantity) {
+}
---
+++ gc-coffee/src/test/java/com/example/gccoffee/model/EmailTest.java
@@ -0,0 +1,28 @@
+package com.example.gccoffee.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmailTest {
+
+    @Test
+    void testInvalidEmail() {
+        assertThrows(IllegalArgumentException.class,()->{
+            new Email("hello");
+        });
+    }
+
+    @Test
+    void testValidEmail() {
+        Email email = new Email("hello@gmail.com");
+        assertEquals("hello@gmail.com", email.getAddress());
+    }
+
+    @Test
+    void testEqEmail() {
+        Email email = new Email("hello@gmail.com");
+        Email email2 = new Email("hello@gmail.com");
+        assertEquals(email, email2);
+    }
+}
