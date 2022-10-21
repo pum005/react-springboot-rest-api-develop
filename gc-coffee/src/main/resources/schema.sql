d260ad5b480db68a21247dd08ca5dc3289f226cf
si hwan <pum005@naver.com>
2022-10-21 20:10:42 +0900

    feat : reviews 테이블 DDL 추가

--- gc-coffee/src/main/resources/schema.sql
+++ gc-coffee/src/main/resources/schema.sql
@@ -34,3 +34,15 @@ CREATE TABLE order_items
     CONSTRAINT fk_order_items_to_order FOREIGN KEY (order_id) REFERENCES orders (order_id) ON DELETE CASCADE,
     CONSTRAINT fk_order_items_to_product FOREIGN KEY (product_id) REFERENCES products (product_id)
 );
+
+CREATE TABLE reviews
+(
+    review_id    BINARY(16) PRIMARY KEY,
+    product_id   BINARY(16) NOT NULL,
+    title        VARCHAR(20) NOT NULL,
+    content      VARCHAR(20) NOT NULL,
+    created_at   datetime(6) NOT NULL,
+    updated_at   datetime(6)  DEFAULT NULL
+
+    CONSTRAINT fk_order_items_to_product FOREIGN KEY (product_id) REFERENCES products (product_id) ON DELETE CASCADE
+);
